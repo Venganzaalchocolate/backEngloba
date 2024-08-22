df4744856c0036fb51260fc5d81792bcbdd7b515
venganzaalchocolate <venganzaalchocolate@gmail.com>
2024-08-22 13:46:51 +0200

    filtrar por oferta

--- src/controllers/userCvController.js
+++ src/controllers/userCvController.js
@@ -42,7 +42,7 @@ const getUserCvs = async (req, res) => {
     if (req.body.work_schedule && req.body.work_schedule.length > 0) filters["work_schedule"] = { $in: req.body.work_schedule };
     if (req.body.studies && req.body.studies.length > 0) filters["studies"] = { $in: req.body.studies };
     
-    if (req.body.offer) filters["offer._id"] = req.body.offer;
+    if (req.body.offer) filters["offer"] = req.body.offer;
 
     if (req.body.users) filters["_id"]={ $in: req.body.users }
 
